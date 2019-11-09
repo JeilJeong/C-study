c173928b37b46e7a61b1db88752181ed1b0148f2
JeilJeong <wjdwpdlf12@gmail.com>
2019-11-09 16:30:21 +0900

    Add NULL test.c & Update copy.cpp

---
+++ NULL test.c
@@ -0,0 +1,17 @@
+/*
+#include <stdio.h>
+
+int main() {
+	int a1 = NULL;
+	int a2 = 0;
+	int *a3 = NULL;
+	int *a4 = 0;
+	
+	char b1 = 0;
+	char b2 = '\0';
+	char b3 = NULL;
+	char b4 = 'nul';
+
+	printf("%c\n", b4);
+}
+*/
